c94a5cc8b261b41b260a1337000f7bf517ade1aa
maxkleb <maxkleb@gmail.com>
2014-09-26 12:27:45 +0300

    Main

---
+++ src/Server/ClientListener.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Server;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author MaxKleb
+ */
+class ClientListener extends Thread {
+    private Socket cl;
+    private String fileName;
+    
+    public ClientListener(Socket client) {
+        super();
+        cl = client;
+    }
+
+  public void run(){
+       BufferedReader in;
+       PrintWriter out;
+        try {
+            in = new BufferedReader(new InputStreamReader(cl.getInputStream()));
+            String msg,fileName = "";
+            while ((msg = in.readLine()) != null){
+                 
+                 if(msg.contains("<dist>")){
+                   fileName = msg.substring(6);
+                   System.out.println(fileName);
+                    // in.close();
+                     break;
+                }
+            }
+            
+            File myFile = new File (fileName);
+            byte [] mybytearray = new byte [(int)myFile.length()];
+            System.out.println("File size is "+(int)myFile.length());
+            FileInputStream fis = new FileInputStream(myFile);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            bis.read(mybytearray, 0, mybytearray.length);
+
+            OutputStream os = cl.getOutputStream();
+            System.out.println("Sending file");
+
+            os.write(mybytearray, 0, mybytearray.length);
+            /*
+            out = new PrintWriter(cl.getOutputStream(), true);
+            out.println("<System>:<middle>");
+            in = new BufferedReader(new InputStreamReader(cl.getInputStream())); 
+                    String msg;
+                 
+                     while ((msg = in.readLine()) != null){
+                         System.out.println(msg);
+                     if(msg.contains("<System>:<Continue>")){
+                         bis.read(mybytearray, mybytearray.length/2, mybytearray.length);
+                         os.write(mybytearray, mybytearray.length/2, mybytearray.length);}
+                     else{
+                         cl.close();
+                         break;
+                     
+                     }
+                     }
+            */
+            os.flush();
+            cl.close();
+                 //}
+                     
+             
+              } catch (IOException ex) {
+            Logger.getLogger(ClientListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }
+    
+}
